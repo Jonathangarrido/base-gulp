0b3a7de37125f8d14b4b7cc4ca150014a33661e5
jonathan <jonathan.garrido1988@gmail.com>
2016-08-19 18:57:34 -0300

    varios componentes en el scss

--- src/js/src/main.js
+++ src/js/src/main.js
@@ -6,12 +6,33 @@
 
   active();
 
+
   
 
 })();
 
 function active(){
   console.log('inicio');
+  tab();
 }
 
+function tab(){
+  var $tab = $('.tab a');
+  var $tabGroup = $('.tab__conte').children('div');
+  var $target;
 
+  $('.tabs .tab:first-child a').addClass('indicator');
+  $('.tab__conte > div:first-of-type').show();
+
+  $tab.click(function(){
+    
+    $target = $(this).attr('href');
+
+    $($tab).removeClass('indicator');
+    $(this).addClass('indicator');
+     
+
+     $($tabGroup).hide();
+     $($target).show();
+  });
+}
